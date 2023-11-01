ca7ce2b0c4cfa90efb1baa20701a7bdd355563e0
ederson23 <ederson@webfloat.com.br>
2023-11-01 19:01:45 -0300

    Entrega 2 - ate visualizar usuario

---
+++ README.md
@@ -0,0 +1,10 @@
+# Cliente - Matéria Sistemas Distribuídos
+
+Para executar é necessário instalar a gui javafx, e o gerenciador de dependencias maven
+(normalmente pré-instalado nas IDES), ambas as instalações são diferentes dependendo 
+da IDE (Eclipse, NetBeans, IntelliJ) usada, porém a instalação é trivial com uma simples pesquisa.
+
+Para executar o projeto é necessário primeiro executar um servidor (não envio o código pois não
+consegui finalizá-lo) e iniciá-lo em uma porta aberta da rede (por convenção no intervalo [22000,25000],
+na sequência, executar o cliente (provido neste projeto) e conectar usando o IPV4 da máquina em que está
+o servidor e a porta escolhida anteriormente.
--- src/main/java/sistemasdistribuidos/cliente/ClientController.java
+++ src/main/java/sistemasdistribuidos/cliente/ClientController.java
@@ -2,13 +2,11 @@ package sistemasdistribuidos.cliente;
 
 import javafx.concurrent.Task;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.HBox;
 
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import org.apache.commons.codec.digest.DigestUtils;
 
 import java.security.MessageDigest;
@@ -22,13 +20,7 @@ import org.json.JSONException;
 
 public class ClientController {
     @FXML
-    private Label welcomeText;
-
-    @FXML
-    private Label connectionIPLabel;
-
-    @FXML
-    private Label connectionPortLabel;
+    private Label pageTitle;
 
     @FXML
     private TextField connectionIP;
@@ -37,18 +29,34 @@ public class ClientController {
     private TextField connectionPort;
 
     @FXML
-    private Button helloButton;
+    private HBox loginBox;
 
     @FXML
-    private HBox loginBox;
+    private HBox connectBox;
+
+    @FXML
+    private VBox registerBox;
+
+    @FXML
+    private HBox profileBox;
 
     @FXML
     private TextField emailField;
 
     @FXML
     private PasswordField passwordField;
+
+    @FXML
+    private TextField register_name;
+
+    @FXML
+    private TextField register_email;
+
+    @FXML
+    private PasswordField register_password;
     private boolean connected = false;
 
+    User user;
     private String token;
     private Socket clientSocket;
 
@@ -62,38 +70,10 @@ public class ClientController {
                     int serverPort = Integer.parseInt(connectionPort.getText());
                     clientSocket = new Socket(serverAddress, serverPort);
 
-                    loginBox.setVisible(true);
-                    connectionIP.setVisible(false);
-                    connectionPort.setVisible(false);
-                    helloButton.setVisible(false);
-                    connectionIPLabel.setVisible(false);
-                    connectionPortLabel.setVisible(false);
+                    goToLogin();
+                    goToRegister();
+
                     connected = true;
-
-                    String response = receiveMessageFromServer();
-                    if (response != null) {
-                        Platform.runLater(() -> {
-                            try {
-                                JSONObject jsonResponse = new JSONObject(response);
-
-                                boolean error = jsonResponse.getBoolean("error");
-                                String message = jsonResponse.getString("message");
-
-                                if (!error) {
-                                    token = jsonResponse.getJSONObject("data").getString("token");
-                                    welcomeText.setText("Logado com sucesso");
-                                    System.out.println("Resposta do servidor: " + response);
-                                    System.out.println("Token recebido: " + token);
-                                } else {
-                                    welcomeText.setText("Login failed: " + message);
-                                    System.out.println("Server Response: " + message);
-                                }
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                                welcomeText.setText("Error parsing JSON response");
-                            }
-                        });
-                    }
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
@@ -110,6 +90,72 @@ public class ClientController {
         return DigestUtils.md5Hex(password).toUpperCase();
     }
 
+    private void setTitle(String str) {
+        Platform.runLater(() -> {
+            pageTitle.setText(str); // Update the label on the JavaFX Application Thread
+        });
+    }
+
+    private void goToConnection() {
+        setTitle("Conectar a servidor");
+        connectBox.setVisible(true);
+        connectBox.setManaged(true);
+        profileBox.setVisible(false);
+        profileBox.setManaged(false);
+        loginBox.setVisible(false);
+        loginBox.setManaged(false);
+        registerBox.setVisible(false);
+        registerBox.setManaged(false);
+    }
+    @FXML
+    private void goToLogin() {
+        setTitle("Faca seu login");
+        connectBox.setVisible(false);
+        connectBox.setManaged(false);
+        profileBox.setVisible(false);
+        profileBox.setManaged(false);
+        loginBox.setVisible(true);
+        loginBox.setManaged(true);
+        registerBox.setVisible(false);
+        registerBox.setManaged(false);
+    }
+
+    private void goToProfile() {
+        setTitle("Perfil/Opções");
+        connectBox.setVisible(false);
+        connectBox.setManaged(false);
+        profileBox.setVisible(true);
+        profileBox.setManaged(true);
+        loginBox.setVisible(false);
+        loginBox.setManaged(false);
+        registerBox.setVisible(false);
+        registerBox.setManaged(false);
+    }
+
+    private void goToRegister() {
+        setTitle("Faça seu autocadastro");
+        connectBox.setVisible(false);
+        connectBox.setManaged(false);
+        profileBox.setVisible(false);
+        profileBox.setManaged(false);
+        loginBox.setVisible(false);
+        loginBox.setManaged(false);
+        registerBox.setVisible(true);
+        registerBox.setManaged(true);
+    }
+
+    private void showWarning(String message) {
+        Alert alert = new Alert(Alert.AlertType.WARNING);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+
+    private void showUser(User user) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Usuário:");
+        alert.setContentText(user.displayUser());
+        alert.showAndWait();
+    }
     @FXML
     protected void onLoginClick() {
         if (connected) {
@@ -119,17 +165,141 @@ public class ClientController {
             String hashedPassword = hashPasswordMD5(password);
             String loginRequest = "{ \"action\": \"login\", \"data\": { \"email\": \"" + email + "\", \"password\": \"" + hashedPassword + "\" } }";
 
-            System.out.println("Mandando JSON para servidor: " + loginRequest);
+            System.out.println("Mandando para servidor: " + loginRequest);
             sendMessageToServer(loginRequest);
+
+            String response = receiveMessageFromServer();
+            if (response != null) {
+                Platform.runLater(() -> {
+                    try {
+                        JSONObject jsonResponse = new JSONObject(response);
+
+                        String action = jsonResponse.getString("action");
+                        boolean error = jsonResponse.getBoolean("error");
+                        String message = jsonResponse.getString("message");
+
+                        if (!error) {
+                            token = jsonResponse.getJSONObject("data").getString("token");
+
+                            clearLoginFields();
+                            goToProfile();
+                            System.out.println("Resposta do servidor: " + response);
+//                            System.out.println("Token recebido: " + token);
+                        } else {
+                            showWarning(message);
+                            System.out.println("Server Response: " + response);
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                        System.out.println("Error parsing JSON response");
+                    }
+                });
+            }
         } else {
         }
     }
 
+    private void clearRegisterFields() {
+        register_password.clear();
+        register_email.clear();
+        register_name.clear();
+    }
+
+    private void clearLoginFields() {
+        emailField.clear();
+        passwordField.clear();
+    }
     @FXML
-    protected void onLogoutClicked() {
+    protected void onSelfRegisterClick() {
+        goToRegister();
+    }
+    @FXML
+    protected void onRegisterClick() {
+        if (connected) {
+            String name = register_name.getText();
+            String email = register_email.getText();
+            String password = register_password.getText();
+
+            String hashedPassword = hashPasswordMD5(password);
+            String registerRequest = "{ \"action\": \"autocadastro-usuario\", \"data\": {  \"name\": \"" + name + "\", \"email\": \"" + email + "\", \"password\": \"" + hashedPassword + "\" } }";
+
+            System.out.println("Mandando para servidor: " + registerRequest);
+            sendMessageToServer(registerRequest);
+
+            String response = receiveMessageFromServer();
+            if (response != null) {
+                Platform.runLater(() -> {
+                    try {
+                        JSONObject jsonResponse = new JSONObject(response);
+
+                        String action = jsonResponse.getString("action");
+                        boolean error = jsonResponse.getBoolean("error");
+                        String message = jsonResponse.getString("message");
+
+                        if (!error && action.equals("autocadastro-usuario")) {
+                            clearRegisterFields();
+                            goToLogin();
+                        } else {
+                            showWarning(message);
+                        }
+                        System.out.println("Resposta do servidor: " + response);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                        System.out.println("Error parsing JSON response");
+                    }
+                });
+            }
+        } else {
+            showWarning("Erro de conexão!");
+        }
+    }
+
+    @FXML
+    protected void onMyDataClick() {
+        if (connected) {
+            String myDataRequest = "{ \"action\": \"pedido-proprio-usuario\", \"data\": {  \"token\": \"" + token + "\"} }";
+
+            System.out.println("Mandando para servidor: " + myDataRequest);
+            sendMessageToServer(myDataRequest);
+
+            String response = receiveMessageFromServer();
+            if (response != null) {
+                Platform.runLater(() -> {
+                    try {
+                        JSONObject jsonResponse = new JSONObject(response);
+
+                        String action = jsonResponse.getString("action");
+                        boolean error = jsonResponse.getBoolean("error");
+                        String message = jsonResponse.getString("message");
+
+                        if (!error && action.equals("pedido-proprio-usuario")) {
+                            user = new User(
+                                    Integer.parseInt(jsonResponse.getJSONObject("data").getString("id")),
+                                    jsonResponse.getJSONObject("data").getString("name"),
+                                    jsonResponse.getJSONObject("data").getString("email"),
+                                    token
+                                    );
+                            user.setAdmin(jsonResponse.getJSONObject("data").getString("type").equals("admin"));
+                            showUser(user);
+                        } else {
+                            showWarning(message);
+                        }
+                        System.out.println("Resposta do servidor: " + response);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                        System.out.println("Error parsing JSON response");
+                    }
+                });
+            }
+        } else {
+            showWarning("Erro de conexão!");
+        }
+    }
+    @FXML
+    protected void onLogoutClick() {
         if (connected) {
             String logoutRequest = "{ \"action\": \"logout\", \"data\": { \"token\": \"" + token + "\" } }";
-            System.out.println("Mandando JSON para servidor: " + logoutRequest);
+            System.out.println("Mandando para servidor: " + logoutRequest);
             sendMessageToServer(logoutRequest);
 
             String response = receiveMessageFromServer();
@@ -142,22 +312,31 @@ public class ClientController {
                         String message = jsonResponse.getString("message");
 
                         if (!error) {
-                            welcomeText.setText("Logout efetuado com sucesso!");
                             System.out.println("Resposta do servidor: " + response);
 
-                            Platform.exit();
+                            emailField.setText("");
+                            passwordField.setText("");
+
+                            if (clientSocket != null && !clientSocket.isClosed()) {
+                                try {
+                                    clientSocket.close();
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                            goToConnection();
                         } else {
-                            welcomeText.setText("logout falhou: " + message);
+                            showWarning(message);
                             System.out.println("Resposta do servidor: " + response);
                         }
                     } catch (JSONException e) {
                         e.printStackTrace();
-                        welcomeText.setText("Error parsing JSON response");
+                        System.out.println("Error parsing JSON response");
                     }
                 });
             }
         } else {
-            welcomeText.setText("Conexão falhou");
+            showWarning("Conexão falhou");
         }
     }
 
